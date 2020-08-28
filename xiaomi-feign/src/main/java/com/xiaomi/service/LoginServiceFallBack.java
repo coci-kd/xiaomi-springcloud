7ec85a8fae2d624da434b4fa19944516f2f26cc2
coci-kd <1804834408@qq.com>
2020-08-28 20:27:55 +0800

    提添加验证码登录的熔断器

--- xiaomi-feign/src/main/java/com/xiaomi/service/LoginServiceFallBack.java
+++ xiaomi-feign/src/main/java/com/xiaomi/service/LoginServiceFallBack.java
@@ -5,6 +5,8 @@ import entity.UserBean;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Map;
+
 
 @RequestMapping("/error")
 @Component
@@ -14,6 +16,18 @@ public class LoginServiceFallBack implements LoginServiceFeign{
     @Override
     public UserBean findUserInfoByAccount(String account) {
         System.out.println("熔断处理，测试方法");
+        return null;
+    }
+
+    @Override
+    public Map sendSms(String phone) throws Exception {
+        System.out.println("熔断处理，发送验证码方法");
+        return null;
+    }
+
+    @Override
+    public Map sendMsg(String phone, String checkCode) {
+        System.out.println("熔断处理，验证码登录方法");
         return null;
     }
 }
